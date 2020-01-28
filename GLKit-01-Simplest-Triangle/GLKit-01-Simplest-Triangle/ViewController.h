50106647c8ff0f3ec565b952f6c34eea1ae22399
Daniate <daniate@126.com>
2020-01-28 12:31:20 +0800

    最简单的三角形

---
+++ GLKit-01-Simplest-Triangle/GLKit-01-Simplest-Triangle/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  GLKit-01-Simplest-Triangle
+//
+//  Created by Daniate on 2020/1/28.
+//  Copyright © 2020 Daniate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@import OpenGLES;
+@import GLKit;
+
+@interface ViewController : GLKViewController
+
+
+@end
+
